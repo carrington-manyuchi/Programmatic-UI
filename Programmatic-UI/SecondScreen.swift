682ebe33d4b719aca0f81f36400079b1b72de3be
Carrington <Manyuchi>
2023-03-07 18:20:18 +0200

    Programmatic UI

---
+++ Programmatic-UI/SecondScreen.swift
@@ -0,0 +1,29 @@
+//
+//  SecondScreen.swift
+//  Programmatic-UI
+//
+//  Created by Carrington Tafadzwa Manyuchi on 2023/03/06.
+//
+
+import UIKit
+
+class SecondScreen: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemTeal
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- Programmatic-UI/ViewController.swift
+++ Programmatic-UI/ViewController.swift
@@ -8,12 +8,47 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let nextButton = UIButton()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        view.backgroundColor = .systemBackground
+        setupButton()
+        
+        title = "First Screen"
+        navigationController?.navigationBar.prefersLargeTitles = true
+        
     }
-
-
+    
+    
+    func setupButton() {
+        
+        view.addSubview(nextButton)
+        
+        nextButton.setTitle("Next", for: .normal)
+        nextButton.backgroundColor = .systemPink
+        
+        nextButton.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            nextButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            nextButton.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            nextButton.widthAnchor.constraint(equalToConstant: 200),
+            nextButton.heightAnchor.constraint(equalToConstant: 50)
+    ])
+        
+        nextButton.addTarget(self, action: #selector(moveNext), for: .touchUpInside)
+        
+    }
+    
+    @objc func moveNext() {
+        
+        let nextScreen = SecondScreen()
+        nextScreen.title = "Second Screen"
+        navigationController?.pushViewController(nextScreen, animated: true)
+        
+    }
 }
 
